3a8d5d0cf190e886b6a0d99165e861e2a96e61e6
CrhistianT <andrewturpo573@gmail.com>
2023-12-24 15:00:24 -0500

    hero start

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,5 +3,34 @@
 </script>
 
 <template>
-  <div>Hello I am a hero</div>
+  <div class="background-img">
+    <div class="hero-message">
+      ALGUN TEXTO ATRACTIVO
+      <div>
+        999999
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+  .background-img {
+    background-image: url('../assets/repartidor2.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: calc(100vh - 56px);
+    width: 100%;
+    display: flex;
+    justify-content: flex-start;
+  }
+
+  .hero-message {
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    color: red;
+    font-size: 24px;
+    font-weight: 700;
+  }
+</style>
