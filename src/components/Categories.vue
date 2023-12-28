ab9a6d6a94078f8894ca8dd166fe08d46d798dde
Crhistian Turpo <andrewturpo573@gmail.com>
2023-12-28 19:27:57 -0300

    categorias

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -4,10 +4,102 @@
 
 <template>
   <section>
-    Aqui vienen categorias
+    <br><b>CATEGORIAS</b><br/>
+      <div>
+          <select>
+            <option>PIZZAS</option>
+            <option>La Tipika</option>
+            <option>Machu Pizza</option>
+            <option>Pizzería Remix</option>
+            <option>Del Búho</option>
+            <option>Tu Candelas</option>
+            <option>Sr. Pizza</option>
+            <option>Sunrise</option>
+          </select>
+          <select>
+            <option>CHIFAS</option>
+            <option>4 Dragones</option>
+            <option>Chifa Lin</option>
+            <option>Chifa Chen Xing</option> 
+          </select>
+          <select>
+            <option>POLLERIAS</option>
+            <option>Parador Chicken</option>
+            <option>La Choza de Oscar</option>
+            <option>El Rancho</option>
+            <option>Polleria el Vecino</option>
+            <option>El Establo de Favio</option>
+          </select>
+          <select>
+            <option>BROSTERIA & SALCHIPAPERIA</option>
+            <option>Palmeras</option>
+            <option>Snoopy</option>
+          </select>
+          <select>
+            <option>CEVICHE</option>
+            <option>Mareas</option>
+            <option>Puerto Arrecife</option>
+          </select>
+          <select>
+            <option>HAMBURGUESAS</option>
+            <option>Bembos</option>
+            <option>Sr. Lechon</option>
+          </select>
+          <select>
+            <option>CHICHARRON</option>
+            <option>La Red</option>
+          </select>
+          <select>
+            <option>PARRILLA</option>
+            <option>La Estancia</option>
+            <option>Casa Grill</option>
+          </select>
+          <select>
+            <option>KANKACHO</option>
+            <option>La Wallatita Kankacho</option>
+          </select>
+          <select>
+            <option>SUSHI</option>
+            <option>Ten Bento</option>
+          </select> 
+          <select>
+            <option>TIMPO</option>
+            <option>Pez de Oro</option>
+          </select>
+          <select>
+            <option>POSTRES</option>
+            <option>Ricos Pan</option>
+            <option>Capriccios</option>
+          </select> 
+      </div>
   </section>
 </template>
 
 <style scoped>
+section{
+  background-color: beige;
+}
+b{
+  font-size: 30px;
+  font-family: fantasy;
+  text-align: right;
+  color: rgba(255, 25, 25, 0.945);
+  background-color: whitesmoke;
+}
+select{
+  font-size: medium;
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: center;
+  background-color: rgb(233, 28, 28);
+  grid-template-columns: 7% 7% 7% 7% 7% 7% 7% 7% 7% 7% 7% 7%;
+  column-gap: 5%;
+  padding: 1rem;
+  margin: 1rem auto;
+  max-width: 1200px;
+}
+option{
+        background:rgba(0, 89, 255, 0.842);
+}
+
 
 </style>
--- src/data/restaurantes.js
+++ src/data/restaurantes.js
@@ -1,7 +1,194 @@
 export const db = [
   {
     id: 1,
-    nombre: "Restaurante 1",
+    nombre: "Pizzería La Tipika",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Hawayana",
+        precio: 16
+      },
+      {
+        id: 2,
+        nombre: "Cariveña",
+        precio: 24
+      },
+      {
+        id: 3,
+        nombre: "Hawayana de Pollo",
+        precio: 33
+      },
+      {
+        id: 4,
+        nombre: "Exotika",
+        precio: 18
+      },
+      {
+        id: 5,
+        nombre: "Tipika Dulce",
+        precio: 20
+      },
+      {
+        id: 6,
+        nombre: "Cariveña Mix",
+        precio: 28
+      },
+      {
+        id: 7,
+        nombre: "Mozzarella",
+        precio: 16
+      },
+      {
+        id: 8,
+        nombre: "Aceituna",
+        precio: 30
+      },
+      {
+        id: 9,
+        nombre: "Jamon",
+        precio: 16
+      },
+      {
+        id: 10,
+        nombre: "Tocino",
+        precio: 25
+      },
+      {
+        id: 11,
+        nombre: "Chorizo",
+        precio: 16
+      },
+      {
+        id: 12,
+        nombre: "Cavanossi",
+        precio: 16
+      },
+      {
+        id: 13,
+        nombre: "Salame",
+        precio: 16
+      },
+      {
+        id: 14,
+        nombre: "Champiñones",
+        precio: 16
+      },
+      {
+        id: 15,
+        nombre: "Peperoni",
+        precio: 16
+      },
+      {
+        id: 16,
+        nombre: "Mixta",
+        precio: 16
+      },
+      {
+        id: 17,
+        nombre: "Mediterranea",
+        precio: 16
+      },
+      {
+        id: 18,
+        nombre: "Peruanita",
+        precio: 16
+      },
+      {
+        id: 19,
+        nombre: "Mexicana",
+        precio: 16
+      },
+      {
+        id: 20,
+        nombre: "Americana",
+        precio: 16
+      },
+      {
+        id: 21,
+        nombre: "Arabe",
+        precio: 25
+      },
+      {
+        id: 22,
+        nombre: "Atenea",
+        precio: 16
+      },
+      {
+        id: 23,
+        nombre: "Tipika Clasica",
+        precio: 18
+      },
+      {
+        id: 24,
+        nombre: "Alemana",
+        precio: 18
+      },
+      {
+        id: 25,
+        nombre: "La Caprichosa",
+        precio: 18
+      },
+      {
+        id: 26,
+        nombre: "La Puneña",
+        precio: 18
+      },
+      {
+        id: 27,
+        nombre: "La Diabla (Jamon, Peperoni, Tocino y Aceitunas negras",
+        precio: 18
+      },
+      {
+        id: 28,
+        nombre: "Duo Tipika",
+        precio: 18
+      },
+      {
+        id: 29,
+        nombre: "Cuartro Estaciones",
+        precio: 18
+      },
+      {
+        id: 30,
+        nombre: "La Parrillera",
+        precio: 18
+      },
+      {
+        id: 31,
+        nombre: "Margarita",
+        precio: 16
+      },
+      {
+        id: 32,
+        nombre: "Napolitana",
+        precio: 16
+      },
+      {
+        id: 33,
+        nombre: "Vegetariana",
+        precio: 18
+      },
+      {
+        id: 34,
+        nombre: "Mix Vegetariana",
+        precio: 20
+      },
+      {
+        id: 35,
+        nombre: "Lazaña de Pollo",
+        precio: 23
+      },
+      {
+        id: 36,
+        nombre: "Lazaña de Carne",
+        precio: 23
+      },
+    ]
+  },
+  {
+    id: 2,
+    nombre: "Chifa 4 Dragones",
     imagen: "restaurant-image.jpg",
     platos: [
       {
@@ -17,15 +204,479 @@ export const db = [
     ]
   },
   {
-    id: 2,
-    nombre: "Restaurante 2",
+    id: 3,
+    nombre: "Parador Chicken",
     imagen: "restaurant-image.jpg",
     platos: [
       {
         id: 1,
-        nombre: "Causa",
-        precio: 15
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
       }
     ]
-  }
+  },
+  {
+    id: 4,
+    nombre: "Bembos",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 5,
+    nombre: "La chozza de Oscar",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 6,
+    nombre: "Salchipaperia Palmeras",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 7,
+    nombre: "Polleria El Rancho",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 8,
+    nombre: "Chifa Lin",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 9,
+    nombre: "La Wallatita Kankacho",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 10,
+    nombre: "Pizzería Machu pizza",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 11,
+    nombre: "Pizzería Remix",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 12,
+    nombre: "Capriccios",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 13,
+    nombre: "Ricospan",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 14,
+    nombre: "Parrilleria Casa Grill",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 15,
+    nombre: "Polloria el Vecino",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 16,
+    nombre: "Chifa Chen Xing",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 17,
+    nombre: "La Estancia",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 18,
+    nombre: "Chicharroneria la Red",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 19,
+    nombre: "Cevicheria Mareas",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 20,
+    nombre: "Del Búho Pizzería",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 21,
+    nombre: "Brossteria Snuoopy",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 22,
+    nombre: "Ten Bento",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 23,
+    nombre: "El Establo de Favio",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 24,
+    nombre: "Hamburgueseria Sr. Lechon",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 25,
+    nombre: "Thimpo Pez de Oro",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 26,
+    nombre: "Pizzería TuCandelas",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 27,
+    nombre: "Pizzería Sr. Pizza",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 28,
+    nombre: "Cevicheria Puerto Arrecife",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 29,
+    nombre: "Pizzeria Sunrise",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
+  {
+    id: 30,
+    nombre: "Restaurante 30",
+    imagen: "restaurant-image.jpg",
+    platos: [
+      {
+        id: 1,
+        nombre: "Ceviche",
+        precio: 12
+      },
+      {
+        id: 2,
+        nombre: "Lomo Saltado",
+        precio: 11
+      }
+    ]
+  },
 ]
