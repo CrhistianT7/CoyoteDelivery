34011175ea413e4b1af350b1e88e72362fd75625
Tess-Yue <mtbmt21@gmail.com>
2023-12-28 19:54:14 -0500

    estilos

--- src/components/Restaurants.vue
+++ src/components/Restaurants.vue
@@ -11,9 +11,9 @@ const props = defineProps({
 </script>
 
 <template>
-  <div>
+  <div class="restaurant-info">
     <img :src="'/src/assets/restaurantes/' + restaurante?.imagen" class="restaurant-img" :alt="'restaurante imagen'">
-    {{ restaurante?.nombre }}
+    <span>{{ restaurante?.nombre }}</span>
     <div>
       <RouterLink :to="`/restaurante/${restaurante.id}`">Visitar restaurante</RouterLink>
     </div>
@@ -22,8 +22,17 @@ const props = defineProps({
 
 <style scoped>
 .restaurant-img {
-  width: 50px;
-  height: 50px;
+  width: 100px;
+  height: 100px;
   border-radius: 50%;
+}
+.restaurant-info{
+  color: #000;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #000;
+  border-radius: 8px;
+  padding: 6px;
+  align-items: center;
 }
 </style>
